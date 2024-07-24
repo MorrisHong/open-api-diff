df3eea7d094d370758dee7d521f6d035f0fd6a71
Morris Hong <govlmo91@gmail.com>
2024-07-24 11:03:37 +0900

    first

---
+++ src/main/kotlin/kr/gracelove/openapidiffdemo/OpenApiDiffDemoApplication.kt
@@ -0,0 +1,11 @@
+package kr.gracelove.openapidiffdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenApiDiffDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenApiDiffDemoApplication>(*args)
+}
